dd4356df3d3dcb23de74bcba43bfdb6049d2b6ab
saddamhossain <saddamhossain953@gmail.com>
2024-10-02 15:51:58 +0600

    Add project files.

---
+++ BlazorServerManageReconnection/wwwroot/js/CustomScript.js
@@ -0,0 +1,12 @@
+﻿new MutationObserver((mutations, observer) => {
+    // call when any UI chnages occured.
+    if (document.querySelector("#components-reconnect-modal h5 a")) {
+        async function Reconnection() {
+            await fetch(""); // Check the server is available
+            location.reload();
+        }
+        observer.disconnect();
+        Reconnection();
+        setInterval(Reconnection, 1000); // now every 10 seconds, see if the server is back, then reload.
+    }
+}).observe(document.body, { childList: true, subtree: true });
